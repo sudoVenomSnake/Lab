9856108ff1b2ac4120d4eea8bcc110e2c0b844cd
sudoVenomSnake <swastikm26@gmail.com>
2022-11-18 23:39:30 +0530

    Commit

---
+++ jUnitFunctions.java
@@ -0,0 +1,16 @@
+package jUnitTestPackage;
+
+
+public class jUnitFunctions {
+
+	public int addTwo(int a, int b)
+	   {
+	      int sum = a + b;
+	      return sum;
+	   }
+	public String addString(String a, String b)
+	{
+		String sum = a + b;
+		return sum;
+	}
+}
